e25fbf91d7a540b5d75abf37f48eaa94658cc81e
YSJ <ysjhmtb@gmail.com>
2018-09-19 21:01:21 +0900

    postings updates

--- documents/VIVA BRUTE FORCE/ALGORITHM/Kakao 2018/Kakao 2018.md
+++ documents/VIVA BRUTE FORCE/ALGORITHM/Kakao 2018/Kakao 2018.md
@@ -252,3 +252,157 @@ for (let i = 0; i < answer.length; i++) {
 모든 사용자가 마지막 스테이지에 있으므로 4번 스테이지의 실패율은 1이며 나머지 스테이지의 실패율은 0이다.
 
 - [4,1,2,3]
+
+<br>
+
+
+
+```javascript
+/*
+N	stages	                    result
+5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
+4	[4,4,4,4,4]	                [4,1,2,3]
+
+
+실패율은 다음과 같이 정의한다.
+스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+
+
+1)
+일단 실패율을 기록하자. 만일 실패율이 1/8이라면 [1,8]로 저장하자.
+
+2)
+입력받은 실패율들을 바탕으로 통분을 통해서 크기를 비교하자.
+만일 같은 실패율이라면 작은 숫자를 우선 배치하자.
+
+
+
+
+ */
+
+const getFailPro = function (N, stages, target) {
+
+    let numerator = 0;
+    let denominator = 0;
+
+    for (let i = 0; i <= stages.length; i++) {
+
+        if (target == stages[i]) {
+            numerator++;
+        }
+
+        if (target <= stages[i]) {
+            denominator++;
+        }
+    }
+
+    return [numerator, denominator];
+
+};
+
+
+const getCDRArr = function (failArr) {
+
+    let commonDeno = 1;
+    let cdrArr = [];
+
+    for (let i = 0; i < failArr.length; i++) {
+
+        let temp = failArr[i];
+        commonDeno *= temp[1];
+    }
+
+    for (i = 0; i < failArr.length; i++) {
+        let temp = failArr[i];
+        let numerator = temp[0];
+        let denominator = temp[1];
+
+        numerator *= (commonDeno / denominator);
+        temp = [i + 1, numerator, commonDeno];
+        cdrArr.push(temp);
+
+    }
+
+    return cdrArr;
+
+};
+
+const getSortedArr = function (cdrArr) {
+
+    //분자만으로 비교해서 배열에 담자.
+    //이후 같은 분자를 가진 경우를 조사해서 조정하자.
+
+    let results = [];
+
+
+    while (cdrArr.length > 0) {
+
+        let arrIndex = -1;
+        let maxIndex = -1;
+        let maxValue = -1;
+
+        for (let i = 0; i < cdrArr.length; i++) {
+            if (cdrArr[i][1] >= maxValue) {
+                arrIndex = i;
+                maxIndex = cdrArr[i][0];
+                maxValue = cdrArr[i][1];
+            }
+
+        }
+
+        results.push([maxIndex, maxValue]);
+        cdrArr.splice(arrIndex, 1);
+
+    }
+
+
+    for (let i = 0; i < results.length; i++) {
+        for (let j = i + 1; j < results.length; j++) {
+            if ((results[i][1] == results[j][1]) && (results[i][0] > results[j][0])) {
+                let temp = results[i];
+                results[i] = results[j];
+                results[j] = temp;
+            }
+        }
+    }
+
+    let realResults = [];
+    for (let i = 0; i < results.length; i++) {
+        let temp = results[i][0];
+        realResults.push(temp);
+    }
+
+    return realResults;
+
+
+};
+
+
+const solution = function (N, stages) {
+
+    let failArr = [];
+    for (let i = 1; i <= N; i++) {
+        let temp = getFailPro(N, stages, i);
+        failArr.push(temp);
+    }
+
+    let cdrArr = getCDRArr(failArr);
+    let result = getSortedArr(cdrArr);
+
+    return result;
+
+
+};
+
+
+//[ 3, 4, 2, 1, 5 ]
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
+
+//[ 4, 1, 2, 3 ]
+console.log(solution(4, [4, 4, 4, 4, 4]));
+
+
+```
+
+<br><br>
+
--- documents/VIVA BRUTE FORCE/CODING/nodejs/HelloWorld/hello.js
+++ documents/VIVA BRUTE FORCE/CODING/nodejs/HelloWorld/hello.js
@@ -7,28 +7,28 @@ N	stages	                    result
 실패율은 다음과 같이 정의한다.
 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 
-var quotient = Math.floor(y/x);
-var remainder = y % x;
 
+1)
+일단 실패율을 기록하자. 만일 실패율이 1/8이라면 [1,8]로 저장하자.
 
-일단 실패율을 기록하자. 이후 실패율과 숫자 크기라는 두 기준으로 정렬하자.
-이를 위해서는 분수 계산의 결과를 반환하는 함수를 만들어야 한다.
-입력받은 stages의 값들을 하나씩 검사하면서 실패율을 기록하자.
+2)
+입력받은 실패율들을 바탕으로 통분을 통해서 크기를 비교하자.
+만일 같은 실패율이라면 작은 숫자를 우선 배치하자.
+
 
 
 
  */
 
+const getFailPro = function (N, stages, target) {
 
-let getProba = function (N, stages, target) {
-
-    let targetQuan = 0;
+    let numerator = 0;
     let denominator = 0;
 
-    for (let i = 0; i < stages.length; i++) {
+    for (let i = 0; i <= stages.length; i++) {
 
         if (target == stages[i]) {
-            targetQuan++;
+            numerator++;
         }
 
         if (target <= stages[i]) {
@@ -36,19 +36,83 @@ let getProba = function (N, stages, target) {
         }
     }
 
-    let quotient = Math.floor(targetQuan / denominator);
-    let remainder = targetQuan % denominator;
-
-    return [quotient, remainder];
-
+    return [numerator, denominator];
 
 };
 
-let recordFail = function (keyNum, quotient, remainder) {
 
-    this.keyNum = keyNum;
-    this.quotient = quotient;
-    this.remainder = remainder;
+const getCDRArr = function (failArr) {
+
+    let commonDeno = 1;
+    let cdrArr = [];
+
+    for (let i = 0; i < failArr.length; i++) {
+
+        let temp = failArr[i];
+        commonDeno *= temp[1];
+    }
+
+    for (i = 0; i < failArr.length; i++) {
+        let temp = failArr[i];
+        let numerator = temp[0];
+        let denominator = temp[1];
+
+        numerator *= (commonDeno / denominator);
+        temp = [i + 1, numerator, commonDeno];
+        cdrArr.push(temp);
+
+    }
+
+    return cdrArr;
+
+};
+
+const getSortedArr = function (cdrArr) {
+
+    //분자만으로 비교해서 배열에 담자.
+    //이후 같은 분자를 가진 경우를 조사해서 조정하자.
+
+    let results = [];
+
+
+    while (cdrArr.length > 0) {
+
+        let arrIndex = -1;
+        let maxIndex = -1;
+        let maxValue = -1;
+
+        for (let i = 0; i < cdrArr.length; i++) {
+            if (cdrArr[i][1] >= maxValue) {
+                arrIndex = i;
+                maxIndex = cdrArr[i][0];
+                maxValue = cdrArr[i][1];
+            }
+
+        }
+
+        results.push([maxIndex, maxValue]);
+        cdrArr.splice(arrIndex, 1);
+
+    }
+
+
+    for (let i = 0; i < results.length; i++) {
+        for (let j = i + 1; j < results.length; j++) {
+            if ((results[i][1] == results[j][1]) && (results[i][0] > results[j][0])) {
+                let temp = results[i];
+                results[i] = results[j];
+                results[j] = temp;
+            }
+        }
+    }
+
+    let realResults = [];
+    for (let i = 0; i < results.length; i++) {
+        let temp = results[i][0];
+        realResults.push(temp);
+    }
+
+    return realResults;
 
 
 };
@@ -56,16 +120,31 @@ let recordFail = function (keyNum, quotient, remainder) {
 
 const solution = function (N, stages) {
 
+    let failArr = [];
     for (let i = 1; i <= N; i++) {
-
-        let temp = getProba(N, stages, i);
-        let quotient = temp[0];
-        let remainder = temp[1];
-        console.log(quotient + " " + remainder);
+        let temp = getFailPro(N, stages, i);
+        failArr.push(temp);
     }
 
+    let cdrArr = getCDRArr(failArr);
+    let result = getSortedArr(cdrArr);
+
+    return result;
 
 
 };
 
-solution(5, [2, 1, 2, 6, 2, 4, 3, 3]);
+
+//[ 3, 4, 2, 1, 5 ]
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
+
+//[ 4, 1, 2, 3 ]
+console.log(solution(4, [4, 4, 4, 4, 4]));
+
+
+
+
+
+
+
+
