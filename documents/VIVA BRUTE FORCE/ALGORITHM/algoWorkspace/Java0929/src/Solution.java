b7fd9293b072c1c7751eb5c50134c998521ae1ff
YSJ <ysjhmtb@gmail.com>
2018-10-11 21:21:50 +0900

    scraps updates

--- documents/VIVA BRUTE FORCE/ALGORITHM/algoWorkspace/Java0929/src/Solution.java
+++ documents/VIVA BRUTE FORCE/ALGORITHM/algoWorkspace/Java0929/src/Solution.java
@@ -1,140 +1,119 @@
 
 /*
-NHNENT 2018 수익 최대화
-
-주어진 가격 예측 데이터를 기반으로 ‘코인’을 사고 팔아서 얻을 수 있는 최대 이익
-
-
-입력 1)
-5
-1 1 10 5 7
-
-출력 1)
-18 = (9 * 2 - 1) + (2 - 1)
-
-
-입력 2)
-5
-5 4 3 2 1
-
-출력 2)
-0
-
-
-입력 3)
-5
-5 10 5 10 5
-
-출력 3)
-9 = (5 * 2 - 1)
+NHNENT 2018 점령지 확장
 
 
 
+보기 입력 1
+5↵
+A ␣0 ␣0 ␣0 ␣0↵
+0 ␣0 ␣6 ␣0 ␣D↵
+0 ␣0 ␣B ␣0 ␣0↵
+0 ␣C ␣8 ␣0 ␣0↵
+0 ␣0 ␣0 ␣0 ␣0↵
+
+
+출력 1
+3↵
+A ␣A ␣A ␣D ␣D↵
+A ␣A ␣D ␣D ␣D↵
+0 ␣0 ␣B ␣B ␣D↵
+C ␣C ␣C ␣B ␣D↵
+C ␣C ␣C ␣0 ␣D↵
 
 
 
-현재 인덱스에서 어느 지점에서 최대값으로 팔 수 있는지,
-같은 최대값이면 어느 지점이 가장 뒤인지(수수료 감소 목적) 검사한다.
+보기 입력 2
+5↵
+A ␣0 ␣0 ␣0 ␣0↵
+0 ␣0 ␣0 ␣0 ␣0↵
+0 ␣0 ␣0 ␣0 ␣0↵
+0 ␣0 ␣0 ␣0 ␣0↵
+0 ␣0 ␣0 ␣0 ␣B↵
+
+출력 2
+3↵
+A ␣A ␣A ␣A ␣0↵
+A ␣A ␣A ␣0 ␣B↵
+A ␣A ␣0 ␣B ␣B↵
+A ␣0 ␣B ␣B ␣B↵
+0 ␣B ␣B ␣B ␣B↵
+
+
+
+보기 입력 3
+5↵
+A ␣0 ␣0 ␣0 ␣10↵
+0 ␣0 ␣0 ␣10 ␣0↵
+0 ␣0 ␣10 ␣0 ␣0↵
+0 ␣10 ␣0 ␣0 ␣0↵
+10 ␣0 ␣0 ␣0 ␣0↵
+
+출력 3
+4↵
+A ␣A ␣A ␣A ␣A↵
+A ␣A ␣A ␣A ␣0↵
+A ␣A ␣A ␣0 ␣0↵
+A ␣A ␣0 ␣0 ␣0↵
+A ␣0 ␣0 ␣0 ␣0↵
+
+
 
  */
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Solution {
 
 
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public void setLiPos(int[] arr) {
-        int max = 0;
-        int target = -1;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] < arr[j] && max <= arr[j]) {
-                    max = arr[j];
-                    target = j;
-                }
-            }
-
-            if (target != -1) {
-                this.map.put(i, target);
-                max = 0;
-                target = -1;
-            }
-
-        }
-    }
-
-    public int calResult(int[] arr) {
-        int result = 0;
-        List<List<Integer>> checkSto = new ArrayList<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            checkSto.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < arr.length; i++) {
 
 
-            if (this.map.containsKey(i)) {
-                int j = this.map.get(i);
-                List<Integer> temp = checkSto.get(j);
-                temp.add(i);
-                checkSto.set(j, temp);
-            }
 
-        }
+import java.util.*;
 
-        for (int i = 0; i < checkSto.size(); i++) {
-            List<Integer> temp = checkSto.get(i);
-
-            if (temp.size() != 0) {
-                for (int j = 0; j < temp.size(); j++) {
-                    result += (arr[i] - arr[temp.get(j)]);
-                }
-
-                result--;
-            }
-
-
-        }
-
-        return result;
-    }
-
-
+class Solution {
     public static void main(String[] args) {
+        List<List<String>> arr = new ArrayList<>();
 
-        Solution ref = new Solution();
-
-        int size = 5;
-        int[] arr = new int[]{5, 10, 5, 10, 5};
-
-        ref.setLiPos(arr);
-        System.out.println(ref.calResult(arr)); // 9
-
-
-        ref = new Solution();
-        size = 5;
-        arr = new int[]{1, 1, 10, 5, 7};
-
-        ref.setLiPos(arr);
-        System.out.println(ref.calResult(arr)); // 18
-
-        ref = new Solution();
-        size = 5;
-        arr = new int[]{5, 4, 3, 2, 1};
-
-        ref.setLiPos(arr);
-        System.out.println(ref.calResult(arr)); // 0
+        int N = 5;
+        String[] a1 = new String[]{"A", "0", "0", "0", "0"};
+        String[] a2 = new String[]{"0", "0", "6", "0", "D"};
+        String[] a3 = new String[]{"0", "0", "B", "0", "0"};
+        String[] a4 = new String[]{"0", "C", "8", "0", "0"};
+        String[] a5 = new String[]{"0", "0", "0", "0", "0"};
 
 
+        arr.add(Arrays.asList(a1));
+        arr.add(Arrays.asList(a2));
+        arr.add(Arrays.asList(a3));
+        arr.add(Arrays.asList(a4));
+        arr.add(Arrays.asList(a5));
     }
+
+
+
+
+    /*
+    게임을 종료해야 하는지를 검사.
+    각 위치가 국가로 위치해 있거나,
+    반환 글자가 "NO"이면 종료해도 된다.
+    종료 가능하면 true 반환하자.
+
+    */
+
+
+    /*
+    어느 방향을 탐색할지 결정되면, 점령을 할 국가가 있는지,
+    있다면 어느 국가인지 반환한다.
+    국가가 존재한다면 해당 알파벳을 없다면 "NO"를 반환한다.
+    */
+
+
+
+
+    /*
+    현재 위치에서 어느 방향을 탐색하고 어느 방향을 탐색하지 말지를 결정해야 한다.
+   	결정된 결과를 반환한다. 이 메소드가 실행 전에 현재 위치에 글자가 존재한다면
+   	실행할 필요가 없다,
+    */
 
 
 }
