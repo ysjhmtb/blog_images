1f49f417a00f59351b34819d17466b9962f5b5e3
YSJ <ysjhmtb@gmail.com>
2018-10-22 23:59:58 +0900

    posting

--- documents/VIVA BRUTE FORCE/CODING/C++/work1020/ground/main.cpp
+++ documents/VIVA BRUTE FORCE/CODING/C++/work1020/ground/main.cpp
@@ -1,59 +1,28 @@
-
 /*
- *  C++ 더 쉽게, 더 깊게  116페이지
+ *  윤성우 열혈 C++ 90page
  *
- *  동전 던지기를 흉내 내는 프로그램을 작성하라. 이 프로그램을 여러 번 실행한 뒤
- *  결과가 한쪽으로 치우치치 않는지 확인하라.
+ *  다음의 상수선언을 보자.
+ *  const int num = 12;
+ *
+ *  포인터 변수를 선언해서 위 변수를 가리키게 해보자.
+ *  그리고 이 포인터 변수를 참조하는 참조자를 하나 선언하자.
+ *  마지막으로 이렇게 선언된 포인터 변수와 참조자를 이용해서 num에 저장된 값을 출력하는 예제를 완성해 보자.
  */
 
-
-
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
 
-/*
- *  0~9     범위를 원하면 / 10
- *  0~10    범위를 원하면 / 11
- */
-
-int randRange(int low, int high) {
-
-    return rand() % (high - low + 1) + low;
-}
-
-
 int main() {
 
-    srand(time(NULL));
+    const int num = 12;
 
-    int frontCount = 0;
-    int rearCount = 0;
-    int errorCount = 0;
+    const int *ptr = &num;
+    const int *&ref = ptr;
 
+    cout << *ptr << "   " << *ref << endl;
 
-    for (int i = 0; i < 1000; i++) {
-
-        int temp = randRange(0, 1);
-
-        if (temp == 0) {
-            frontCount++;
-        } else if (temp == 1) {
-            rearCount++;
-        } else {
-            errorCount++;
-        }
-
-        cout << temp << endl;
-    }
-
-    cout << "결과 확인 " << endl;
-    cout << "앞면 : " << frontCount << endl;
-    cout << "뒷면 : " << rearCount << endl;
-    cout << "에러 : " << errorCount << endl;
 
     return 0;
 }
