481ba46182c2e0bd2f1990dab64e5d450cdffabb
YSJ <ysjhmtb@gmail.com>
2018-11-10 23:29:42 +0900

    ongoing React

--- ReactNodeGround/ground1/react-tutorials/contact-app/src/App.js
+++ ReactNodeGround/ground1/react-tutorials/contact-app/src/App.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
-import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
 
   id = 0;
 
   state = {
-    information : [],
+    information: [],
   }
 
   handleCreate = (data) => {
 
+    // 자식 클래스에서 state를 전달받는다.
+    console.log(data);
+
     const {information} = this.state;
-    
     this.setState({
       information: information.concat({
         ...data,
-        id: this.id++
+        id: this.id++,
       })
-    });
-
+    }); 
   }
-
 
   render() {
     return (
-      <div className="App">
-        <PhoneForm onCreate = {this.handleCreate} />
-        <PhoneInfoList data = {this.state.information}/>
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
       </div>
     );
   }
--- ReactNodeGround/ground1/react-tutorials/contact-app/src/components/PhoneForm.js
+++ ReactNodeGround/ground1/react-tutorials/contact-app/src/components/PhoneForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class PhoneForm extends Component {
 
-
     state = {
         name: '',
         phone: '',
@@ -14,35 +13,38 @@ class PhoneForm extends Component {
         });
     }
 
+    
     handleSubmit = (e) => {
+        // form 속성이 submit 버튼을 누르면 새로고침을 하게 된다. 이를 방지할 필요가 있다.
         e.preventDefault();
-        this.props.onCreate(this.state);
+        // 부모 클래스에게 state가 전달된다.
+        this.props.onCreate(this.state);  
+        // 등록하고 나면 초기화
         this.setState({
-            name:'',
-            phone:'',
-        })
+            name: '',
+            phone: '',
+        }) 
+        
     }
 
     render() {
-        return ( 
-            <form onSubmit = {this.handleSubmit}>
+        return (
+            <form onSubmit={this.handleSubmit}> 
                 <input 
-                    name = "name"
-                    placeholder = "이름" 
-                    onChange = {this.handleChange} 
-                    value = {this.state.name} 
-                />
+                    name="name"
+                    placeholder="이름" 
+                    onChange={this.handleChange} 
+                    value={this.state.name} />
 
                 <input 
-                    name = "phone"
-                    placeholder = "전화번호" 
-                    onChange = {this.handleChange}
-                    value = {this.state.phone}
-                />
+                    name="phone"
+                    placeholder="전화번호" 
+                    onChange={this.handleChange}
+                    value={this.state.phone} />
 
                 <button type="submit">등록</button>
 
-               
+                 
             </form>
         );
     }
