7028cb99459b66b353d827be2cab30c5d9874ee4
YSJ <ysjhmtb@gmail.com>
2018-11-29 00:21:23 +0900

    React

--- CodeGround/Nomad/ReactJS로 웹 서비스 만들기/movie_app/src/Movie.js
+++ CodeGround/Nomad/ReactJS로 웹 서비스 만들기/movie_app/src/Movie.js
@@ -2,43 +2,32 @@
  import PropTypes from 'prop-types';
  import './Movie.css';
 
-class Movie extends Component{
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-    }
 
-    render(){
-
-        
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-        
-    }
+function Movie ({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-// class MoviePoster extends Component{
 
-//     static propTypes = {
-//         poster: PropTypes.string.isRequired
-//     }
-
-//     render(){
-//         return (
-//             <img src = {this.props.poster}/>
-//         );
-//     }
-// }
 
 function MoviePoster({poster}){
     return(
         <img src={poster} alt="Movie Poster" />
     )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
