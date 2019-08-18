71549791cb2fe875711d5e04902cf04a2e858e4b
yun <yun@yunui-MacBook-Pro.local>
2019-08-18 19:06:29 +0900

    unfinished data access

---
+++ code/guide/gsaccessingdatamysql/src/main/java/hello/MainController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/demo")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email){
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "saved";
+
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+}
