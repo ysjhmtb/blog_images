fa755e97d1521683c30e2478a66b978c90b13301
yun <yun@yunui-MacBook-Pro.local>
2019-09-14 22:56:14 +0900

    accepted

--- code/LeetCode/LeetPython/topKFrequentWords.py
+++ code/LeetCode/LeetPython/topKFrequentWords.py
@@ -9,16 +9,37 @@ Output: ["the", "is", "sunny", "day"]
 
 Try to solve it in O(n log k) time and O(n) extra space.
 
-
-Count the frequency of each word, and sort the words with a custom ordering relation
-that uses these frequencies. Then take the best k of them.
-
+Accepted
 '''
-
 
 # class Solution:
 #     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+import heapq
+
 
 class Solution:
     def topKFrequent(self, words, k):
-       return
+
+        wordDict = {}
+        for word in words:
+            if word in wordDict:
+                wordDict[word] += 1
+            else:
+                wordDict[word] = 1
+
+        heap = []
+        for key, value in wordDict.items():
+            heapq.heappush(heap, (-value, key))
+
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+
+        return result
+
+
+words = ["i", "love", "leetcode", "i", "love", "coding"]
+k = 2
+
+ref = Solution()
+print(ref.topKFrequent(words, k))
