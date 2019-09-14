78a326d15541ddfb448e8e6e42e67ca651d4520e
yun <yun@yunui-MacBook-Pro.local>
2019-09-14 18:08:45 +0900

    accepted

--- code/LeetCode/LeetPython/topKFrequentElements.py
+++ code/LeetCode/LeetPython/topKFrequentElements.py
@@ -7,18 +7,36 @@ Output: [1,2]
 Input: nums = [1], k = 1
 Output: [1]
 
-
-
-
-1) The first step is to build a hash map element -> its frequency. O(N)
-2) The second step is to build a heap. O(Nlog(k))
-3) The last step to build an output list has O(klog(k)) time complexity.
+Accepted
 '''
-
 
 # class Solution:
 #     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+import heapq
+
+
 class Solution:
     def topKFrequent(self, nums, k):
-        return
+        hashMap = {}
+        for num in nums:
+            if num in hashMap:
+                hashMap[num] += 1
+            else:
+                hashMap[num] = 1
+
+        heap = []
+        for key, value in hashMap.items():
+            heapq.heappush(heap, (-value, key))
+
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+
+        return result
+
+
+nums1 = [1, 1, 1, 2, 2, 3]
+k1 = 2
+ref = Solution()
+print(ref.topKFrequent(nums1, k1))
