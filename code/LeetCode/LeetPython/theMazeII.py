46ae92f2ae30adfc7ceddb083a01e60ede65a756
yun <yun@yunui-MacBook-Pro.local>
2019-09-15 20:02:23 +0900

    a problem to solve

---
+++ code/LeetCode/LeetPython/theMazeII.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/the-maze-ii/
+
+'''
+
+
+# class Solution:
+#     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+
+class Solution:
+    def shortestDistance(self, maze, start, destination):
+        return
