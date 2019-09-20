e5b87f2c6222e2dd9ed31a91ef91fd5d635851bb
yun <yun@yunui-MacBook-Pro.local>
2019-09-20 20:56:21 +0900

    accepted

---
+++ code/LeetCode/LeetJava/src/LeetJava.java
@@ -0,0 +1,11 @@
+
+
+public class LeetJava {
+    public static void main(String[] args) {
+        TwoSum ref = new TwoSum();
+        int[] result = ref.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println();
+
+    }
+}
+
---
+++ code/LeetCode/LeetJava/src/TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*
+https://leetcode.com/problems/two-sum/
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+Accepted
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
