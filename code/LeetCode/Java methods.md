aa0dc80f765cc1b6a654a2bbd519c5c15f000fd2
yun <yun@yunui-MacBook-Pro.local>
2019-09-28 21:10:14 +0900

    accepted

--- code/LeetCode/Java methods.md
+++
@@ -1,5 +0,0 @@
-- Map - put / containsKey / containsValue / entrySet / isEmpty / keySet / values / remove / get 
-- throw new IllegalArgumentException("No two sum solution");
-- Arrays.fill(one, Integer.MAX_VALUE);
-- 
-
--- code/LeetCode/LeetJava/src/LeetJava.java
+++ code/LeetCode/LeetJava/src/LeetJava.java
@@ -2,18 +2,12 @@
 
 public class LeetJava {
     public static void main(String[] args) {
-
-        int[][] maze = new int[5][5];
-        maze[0] = new int[]{0, 0, 1, 0, 0};
-        maze[1] = new int[]{0, 0, 0, 0, 0};
-        maze[2] = new int[]{0, 0, 0, 1, 0};
-        maze[3] = new int[]{1, 1, 0, 1, 1};
-        maze[4] = new int[]{0, 0, 0, 0, 0};
-        int[] start = new int[]{0, 4};
-        int[] destination = new int[]{3, 2};
-
-        TheMazeII ref = new TheMazeII();
-        System.out.println(ref.shortestDistance(maze,start,destination));
+        MyQueue queue = new MyQueue();
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.peek());
+        System.out.println(queue.pop());
+        System.out.println(queue.empty());
 
     }
 }
