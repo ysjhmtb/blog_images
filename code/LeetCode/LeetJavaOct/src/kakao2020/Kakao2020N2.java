44ca217f9d010830cf8a9f1fe816568ff339297f
yun <yun@yunui-MacBook-Pro.local>
2019-10-14 21:48:03 +0900

    72 sad

--- code/LeetCode/LeetJavaOct/src/kakao2020/Kakao2020N2.java
+++ code/LeetCode/LeetJavaOct/src/kakao2020/Kakao2020N2.java
@@ -9,6 +9,9 @@ https://tech.kakao.com/2019/10/02/kakao-blind-recruitment-2020-round1/
 
 https://programmers.co.kr/learn/courses/30/lessons/60058
 
+
+합계: 72.0 / 100.0
+
  */
 public class Kakao2020N2 {
 
