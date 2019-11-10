d4415cdfdadcc552152626650a3fe9f626fe6c1d
yun <yun@yunui-MacBook-Pro.local>
2019-11-10 23:43:33 +0900

    get data from naver

---
+++ code/ShannonDemon/Shannon1/getdata.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import re
+
+def getDataFromNaver(code):
+    url = 'http://finance.naver.com/item/sise_day.nhn?code='+str(code)
+
+    # 일자 데이터를 담을 df라는 DataFrame 정의
+    df = pd.DataFrame()
+    pattern = re.compile(r'\s+')
+
+    # 1페이지에서 ?페이지의 데이터만 가져오기
+    for page in range(1, 30):
+        pg_url = '{url}&page={page}'.format(url=url, page=page)
+        pg_url = re.sub(pattern, '', pg_url)
+        df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
+
+    # df.dropna()를 이용해 결측값 있는 행 제거
+    df = df.dropna()
+
+    # 한글로 된 컬럼명을 영어로 바꿔줌
+    df = df.rename(
+        columns={'날짜': 'date', '종가': 'close', '전일비': 'diff', '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'})
+
+    # 데이터의 타입을 int형으로 바꿔줌
+    df[['close', 'diff', 'open', 'high', 'low', 'volume']] \
+        = df[['close', 'diff', 'open', 'high', 'low', 'volume']].astype(int)
+
+    # 컬럼명 'date'의 타입을 date로 바꿔줌
+    df['date'] = pd.to_datetime(df['date'])
+
+    # 상위 5개 데이터 확인
+    # print(df.head())
+    return df
+
+# 신한 S&P500 VIX S/T 선물 ETN B 500045
+print(getDataFromNaver(500045).head())
+
+# KODEX 레버리지 122630
+print(getDataFromNaver(122630).head())
+
