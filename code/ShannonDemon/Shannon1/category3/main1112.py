8b1b97ecf66606511f505293a44da628a544a326
yun <yun@yunui-MacBook-Pro.local>
2019-11-21 00:52:36 +0900

    rollover

--- code/ShannonDemon/Shannon1/category3/main1112.py
+++ code/ShannonDemon/Shannon1/category3/main1112.py
@@ -10,7 +10,9 @@ def readCsv(path):
     # rawdata = rawdata.loc[::-1].reset_index(drop=True)
     return rawdata
 
+
 raw = readCsv("./vixcurrent.csv")
+
 
 class Position:
     def __init__(self, vix):
@@ -26,11 +28,19 @@ class Position:
         self.total = self.seed
         self.totalArr = []
         self.totalArr.append(self.total)
+        self.rolloverCount = 0
 
     def rebalance(self, vix):
         if vix > 15:
             diff = (vix * 1000) / self.vixToUSD
             self.total = self.cash + (self.maintainDeposit * self.longCount * diff)
+            if self.rolloverCount < 20:
+                self.rolloverCount += 1
+            elif self.rolloverCount == 20:
+                print("rollover\n\n")
+                self.rolloverCount = 0
+                self.total -= (self.maintainDeposit * self.longCount * diff * 0.05)
+
             self.totalArr.append(self.total)
             self.vixToUSD = vix * 1000
             self.cash = self.total * 0.8
@@ -38,6 +48,13 @@ class Position:
         else:
             diff = (vix * 1000) / self.vixToUSD
             self.total = self.cash + (self.maintainDeposit * self.longCount * diff)
+            if self.rolloverCount < 20:
+                self.rolloverCount += 1
+            elif self.rolloverCount == 20:
+                print("rollover\n\n")
+                self.rolloverCount = 0
+                self.total -= (self.maintainDeposit * self.longCount * diff * 0.05)
+
             self.totalArr.append(self.total)
             self.vixToUSD = vix * 1000
             self.cash = self.total * 0.5
