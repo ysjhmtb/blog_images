fd33565aaf01d1bb5b79e93d63f80d6c1674f929
yun <yun@yunui-MacBook-Pro.local>
2019-11-24 00:57:19 +0900

    ongoing work with unit test

--- code/ShannonDemon/Shannon1/category3/main1122.py
+++ code/ShannonDemon/Shannon1/category3/main1122.py
@@ -14,15 +14,13 @@ Micro E-mini NASDAQ
 계산:[(1572.25-1559.5)/0.25]X10계약X$5
 순이익:USD 2,550
 
-
-
-
 2010 - 2019 데이터
 데이터가 들어오면 롱으로 초기화
 다음 가격이 진입 가격보다 낮으면 청산
 다음 가격이 들어오면 최대 수익 갱신
 다음 가격이 들어오면 최대 수익 80% 하회하면 청산
 청산 후 5거래일 동안은 관망
+
 롤오버 마진콜 수수료까지 고려
 '''
 
@@ -69,6 +67,11 @@ https://ask.fm/cfr0g/answers/152527572970
 가격예측보다 보통 투자금액 배분에 더 신경쓰는 편이 좋습니다.
 https://ask.fm/cfr0g/answers/142743688426
 
+개골님의 최고 수익률 주식은 언제 어떤 것이었나요... #저는_지금이_하한가입니다 — 전 거의 대부분의 전략이 5% 경계안에서 꾸준히 먹는 것이라서, 
+크게 수익률 난 적은 없습니다. http://ask.fm/a/bembfoj8
+오전 1:10 · 2014년 11월 26일
+https://twitter.com/cfr0g/status/537277114689802240
+
 최성훈: 보통 옵션 스캘퍼들이 승률이 90% 정도 나왔음. 그런데 저 같는 경우는 70% 정도. 하루 손실폭은 회사에서 정해 주고,
 그 손실 한계를 넘을 수 없음. 저는 그 손실 한도에 자주 가는 딜러였음. 저는 손실 한도에 많이 가는 반면 수익을 극대화시키는 것에 초점을 둠.
 총 쓸 수 있는 계약이 30계약이면 한번에 들어갈 때 10개씩 3번에 나누어 들어감. 많은 계약을 돈이라고 인식하면 매매를 못하기 때문에 그냥 숫자로 인식함.
@@ -80,3 +83,92 @@ https://ask.fm/cfr0g/answers/142743688426
 박호두 반대매매로 수억을 번 시청자 [박호두 구걸로그] https://youtu.be/RVMoS9DKjQw
 '''
 
+
+class Position1019:
+    # price = 1559.5
+    def __init__(self, price):
+        self.hasLong = True
+        self.startPrice = price  # 1559.5
+        self.totalAsset = 10000
+        self.totalArr = []
+        self.totalArr.append(self.totalAsset)
+        self.CONST_CASH_RATIO = 0.5
+        self.cash = self.totalAsset * self.CONST_CASH_RATIO  # 5,000
+        self.reserveMoney = self.totalAsset - self.cash  # 5,000
+        self.posMoney = self.reserveMoney // 3  # 1,666
+        self.reserveMoney -= self.posMoney  # 5,000 -> 3,334
+        self.entryCount = 1
+        self.maxProfit = 0
+        self.CONST_DEPOSIT = 720
+        self.contractCount = self.posMoney // self.CONST_DEPOSIT  # 2
+        self.posMoney -= (self.contractCount * self.CONST_DEPOSIT)  # 1,666 -> 226
+        self.waitAndSeeFlag = False
+        self.waitAndSeeCount = 0
+        self.CONST_FEE = 2
+        self.rolloverCount = 0
+
+    # price = 1,572.25
+    def liquidatePostion(self, price):
+        # 5,000 + 3,334 + 226 + 204 + 2 * 720 = 10,204
+        # 10,204 - 4 = 10,200
+        self.totalAsset = self.cash + self.reserveMoney + self.posMoney \
+                          + self.calculateProfit(price) \
+                          + (self.contractCount * self.CONST_DEPOSIT) - (2 * self.CONST_FEE)
+        self.totalArr.append(self.totalAsset)
+        self.cash = 0
+        self.reserveMoney = 0
+        self.posMoney = 0
+        self.entryCount = 0
+        self.maxProfit = 0
+        self.contractCount = 0
+        self.waitAndSeeFlag = True
+        self.waitAndSeeCount = 0
+        self.hasLong = False
+        self.startPrice = None
+
+    # price = 1,572.25
+    def calculateProfit(self, price):
+        # 계산:[(1572.25-1559.5)/0.25] X 2계약 X $2 = $204
+        return ((price - self.startPrice) / 0.25) * self.contractCount * 2
+
+    def checkLiquidation(self, price):
+        if price < self.startPrice:
+            return True
+        elif self.calculateProfit(price) < self.maxProfit * 0.8:
+            return True
+        else:
+            return False
+
+    # price = 1559.5
+    def startPosition(self, price):
+        self.hasLong = True
+        self.startPrice = price
+        # 10200 * 0.5 = 5,100
+        self.cash = self.totalAsset * self.CONST_CASH_RATIO
+        # 10,200 - 5,100 = 5,100
+        self.reserveMoney = self.totalAsset - self.cash
+        # 1,700
+        self.posMoney = self.reserveMoney // 3
+        # 5,100 - 1,700 = 3,400
+        self.reserveMoney -= self.posMoney
+        self.entryCount = 1
+        self.maxProfit = 0
+        # 1,700 // 720 = 2
+        self.contractCount = self.posMoney // self.CONST_DEPOSIT
+        # 1,700 - (2 * 720) = 260
+        self.posMoney -= (self.contractCount * self.CONST_DEPOSIT)
+        self.waitAndSeeFlag = False
+        self.waitAndSeeCount = 0
+
+    def checkRollover(self):
+        if self.rolloverCount == 20:
+            self.rolloverCount += 1
+            if self.hasLong:
+                self.totalAsset -= self.totalAsset * 0
+        else:
+            self.rolloverCount += 1
+
+    # price = 1,572.25
+    def updateMaxProfit(self, price):
+        if self.calculateProfit(price) > self.maxProfit:
+            self.maxProfit = self.calculateProfit(price)
--- code/ShannonDemon/Shannon1/category3/main1122UnitTest.py
+++ code/ShannonDemon/Shannon1/category3/main1122UnitTest.py
@@ -1,4 +1,5 @@
 import unittest
+from . import main1122
 
 '''
 https://docs.python.org/3/library/unittest.html 
@@ -8,9 +9,44 @@ assertEqual(ref.total, 20000)
 이런식으로~
 '''
 
+ref = main1122.Position1019(1559.5)
+
+
 class CustomTests(unittest.TestCase):
     def test_runs(self):
-        self.assertEqual(200,200)
+        # 생성자
+        self.assertEqual(ref.startPrice, 1559.5)
+        self.assertEqual(ref.cash, 5000)
+        self.assertEqual(ref.reserveMoney, 3334.0)
+        self.assertEqual(ref.posMoney, 226.0)
+        self.assertEqual(ref.contractCount, 2)
+
+        # calculateProfit
+        self.assertEqual(ref.calculateProfit(1572.25), 204)
+        ref.maxProfit = 204
+
+        # checkLiquidation
+        self.assertTrue(ref.checkLiquidation(1560))
+        self.assertTrue(ref.checkLiquidation(1550))
+        self.assertFalse(ref.checkLiquidation(1580))
+
+        # liquidatePostion
+        ref.liquidatePostion(1572.25)
+        self.assertEqual(ref.totalAsset, 10200.0)
+
+        # startPosition  1559.5
+        ref.startPosition(1559.5)
+        self.assertEqual(ref.cash, 5100.0)
+        self.assertEqual(ref.reserveMoney, 3400.0)
+        self.assertEqual(ref.posMoney, 260)
+        self.assertEqual(ref.contractCount, 2)
+
+        # updateMaxProfit  1572.25
+        ref.updateMaxProfit(1572.25)
+        self.assertEqual(ref.maxProfit, 204)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
