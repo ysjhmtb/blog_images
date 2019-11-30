f6128dc946babc7eb265ee60d701fbfd94f1cea4
yun <yun@yunui-MacBook-Pro.local>
2019-11-30 11:45:15 +0900

    calculation features added

--- code/ShannonDemon/Shannon1/category4/api/position.py
+++ code/ShannonDemon/Shannon1/category4/api/position.py
@@ -49,7 +49,7 @@ class Position_1128:
         self.rolloverCount = 0
 
     def firstPosition(self, cash_ratio, long_ratio, short_ratio):
-        assert (cash_ratio + long_ratio + short_ratio) == 1
+        assert (cash_ratio + long_ratio + short_ratio) == 1.0
         if long_ratio > 0.0:
             self.hasLong = True
         if short_ratio > 0.0:
@@ -63,10 +63,43 @@ class Position_1128:
         if self.hasLong:
             self.longEntryCount += 1
             self.longCount = ((self.reserveMoney * (
-                        long_ratio / (long_ratio + short_ratio))) // 3) // self.CONST_DEPOSIT
+                    long_ratio / (long_ratio + short_ratio))) // 3) // self.CONST_DEPOSIT
             self.reserveMoney -= (self.longCount * self.CONST_DEPOSIT)
         if self.hasShort:
             self.shortEntryCount += 1
             self.shortCount = ((self.reserveMoney * (
-                        short_ratio / (long_ratio + short_ratio))) // 3) // self.CONST_DEPOSIT
+                    short_ratio / (long_ratio + short_ratio))) // 3) // self.CONST_DEPOSIT
             self.reserveMoney -= (self.shortCount * self.CONST_DEPOSIT)
+
+    def getProfit(self, isLong, nextPrice):
+        if isLong:
+            return ((nextPrice - self.longStartPrice) / 0.25) * self.longCount * 0.5
+        else:
+            return ((self.shortStartPrice - nextPrice) / 0.25) * self.shortCount * 0.5
+
+    def checkLiquidation(self, profit):
+        if profit <= (-self.CONST_DEPOSIT):
+            return True
+        elif profit < (self.longMaxProfit + self.shortMaxProfit) * 0.8:
+            return True
+        else:
+            return False
+
+    def liquidate(self):
+        self.hasLong = False
+        self.hasShort = False
+        self.longStartPrice = None
+        self.shortStartPrice = None
+        self.totalAsset = self.cash + self.reserveMoney + self.longMaxProfit + self.shortMaxProfit
+        self.totalArr.append(self.totalAsset)
+        self.cash = 0
+        self.reserveMoney = 0
+        self.longEntryCount = 0
+        self.shortEntryCount = 0
+        self.longMaxProfit = 0
+        self.shortMaxProfit = 0
+        self.longCount = 0
+        self.shortCount = 0
+        self.waitAndSeeFlag = True
+        self.waitAndSeeCount = 1
+        
--- code/ShannonDemon/Shannon1/category4/unit_tests/position_test.py
+++ code/ShannonDemon/Shannon1/category4/unit_tests/position_test.py
@@ -18,8 +18,37 @@ class CustomTests(unittest.TestCase):
         self.assertEqual(ref.shortCount, 0)
         self.assertEqual(ref.reserveMoney, 4280.0)
 
+    def Position_1128_getProfit_long(self):
+        ndqArr = [1559.5]
+        vixArr = [14.2]
+        ref = Position_1128(ndqArr, vixArr)
+        ref.firstPosition(0.5, 0.25, 0.25)
+        # long 1572.25 : ( (1572.25 - 1559.5) / 0.25 ) * 1 * 0.5 = (12.75 / 0.25) * 1 * 0.5 = 25.5
+        self.assertEqual(ref.getProfit(True, 1572.25), 25.5)
+
+    def Position_1128_getProfit_short(self):
+        ndqArr = [1559.5]
+        vixArr = [14.2]
+        ref = Position_1128(ndqArr, vixArr)
+        ref.firstPosition(0.5, 0.0, 0.5)
+        # short 1572.25 : ( (1559.5 - 1572.25) / 0.25 ) * 2  * 0.5 = (-12.75 / 0.25) * 2 * 0.5 = -51
+        self.assertEqual(ref.getProfit(False, 1572.25), -51)
+
+    def Position_1128_checkLiquidation(self):
+        ndqArr = [1559.5]
+        vixArr = [14.2]
+        ref = Position_1128(ndqArr, vixArr)
+        ref.firstPosition(0.5, 0.0, 0.5)
+        ref.longMaxProfit = 900
+        ref.shortMaxProfit = -200
+        self.assertTrue(ref.checkLiquidation(559))
+        self.assertTrue(ref.checkLiquidation(-721))
+
     def test_runs(self):
         self.Position_1128_firstPosition()
+        self.Position_1128_getProfit_long()
+        self.Position_1128_getProfit_short()
+        self.Position_1128_checkLiquidation()
 
 
 if __name__ == '__main__':
