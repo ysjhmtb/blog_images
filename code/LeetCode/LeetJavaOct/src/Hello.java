ebde3b7ad7988d46ed813402cf8d52856b855cdd
yun <yun@yunui-MacBook-Pro.local>
2019-12-19 15:34:49 +0900

    learned from solution

--- code/LeetCode/LeetJavaOct/src/Hello.java
+++ code/LeetCode/LeetJavaOct/src/Hello.java
@@ -4,8 +4,7 @@ import kakao2020.Kakao2020N2;
 
 public class Hello {
     public static void main(String[] args) {
-        N33 ref = new N33();
-        System.out.println(ref.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
-        System.out.println(ref.search(new int[]{5, 1, 3}, 5));
+        N22 ref = new N22();
+        ref.generateParenthesis(3);
     }
 }
